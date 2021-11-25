0f6902b2f788558b1c9ccf6db29c5e14db04367e
tugrulguner <ozgecaylioglu@gmail.com>
2021-11-25 00:01:02 -0500

    First two functions completed

---
+++ Project1/churn_library.py
@@ -0,0 +1,130 @@
+# library doc string
+
+
+# import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def import_data(pth):
+    '''
+    returns dataframe for the csv found at pth
+
+    input:
+            pth: a path to the csv
+    output:
+            df: pandas dataframe
+    '''	
+    df = pd.read_csv(pth)
+    return df
+
+
+def perform_eda(df):
+    '''
+    perform eda on df and save figures to images folder
+    input:
+            df: pandas dataframe
+
+    output:
+            None
+    '''
+    for column_names in df.keys():
+        if df[column_names].dtypes != 'object':
+            plt.figure(figsize=(20,10))
+            plt.ylabel(f'{column_names}')
+            fig_hist = df[column_names].hist().get_figure()
+            fig_hist.savefig(f'./images/eda/{column_names}_hist.png')
+            plt.figure(figsize=(20,10))
+            plt.ylabel(f'{column_names}')
+            fig_value_counts = df[column_names].value_counts('normalize').plot(kind='bar').get_figure()
+            fig_value_counts.savefig(f'./images/eda/{column_names}_value_counts.png')
+            plt.figure(figsize=(20,10))
+            plt.ylabel(f'{column_names}')
+            fig_distplot = sns.distplot(df[column_names]).get_figure()
+            fig_distplot.savefig(f'./images/eda/{column_names}_distplot.png')
+    
+    plt.figure(figsize=(20,10))
+    fig_heatmap = sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2).get_figure()
+    fig_heatmap.savefig(f'./images/eda/{column_names}_heatmap.png')
+
+
+
+def encoder_helper(df, category_lst, response):
+    '''
+    helper function to turn each categorical column into a new column with
+    propotion of churn for each category - associated with cell 15 from the notebook
+
+    input:
+            df: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used for naming variables or index y column]
+
+    output:
+            df: pandas dataframe with new columns for
+    '''
+    pass
+
+
+def perform_feature_engineering(df, response):
+    '''
+    input:
+              df: pandas dataframe
+              response: string of response name [optional argument that could be used for naming variables or index y column]
+
+    output:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    '''
+
+def classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf):
+    '''
+    produces classification report for training and testing results and stores report as image
+    in images folder
+    input:
+            y_train: training response values
+            y_test:  test response values
+            y_train_preds_lr: training predictions from logistic regression
+            y_train_preds_rf: training predictions from random forest
+            y_test_preds_lr: test predictions from logistic regression
+            y_test_preds_rf: test predictions from random forest
+
+    output:
+             None
+    '''
+    pass
+
+
+def feature_importance_plot(model, X_data, output_pth):
+    '''
+    creates and stores the feature importances in pth
+    input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+
+    output:
+             None
+    '''
+    pass
+
+def train_models(X_train, X_test, y_train, y_test):
+    '''
+    train, store model results: images + scores, and store models
+    input:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    output:
+              None
+    '''
+    pass
