7b3a8408291eb75ab0561f4fb133bdaea85a4c1b
tugrulguner <tugrulgunr@gmail.com>
2021-11-28 19:23:36 -0500

    Test and Logging completed

--- Project1/chrun_script_logging_and_test.py
+++ Project1/chrun_script_logging_and_test.py
@@ -30,27 +30,27 @@ def test_import(import_data):
 		raise err
 
 
-# def test_eda(perform_eda):
-#     '''
-#     test_eda to test perform_eda function
-#     '''
+def test_eda(perform_eda):
+    '''
+    test_eda to test perform_eda function
+    '''
     
-#     df = import_data("./data/bank_data.csv")
-#     try:
-#         perform_eda(df)
-#         assert len(os.listdir('./images/eda/') ) > 0
-#         logging.info('Images were created: SUCCESS')
-#     except AssertionError as err:
-#         logging.error("Testing perform_eda: Plots were not created, check your data")
-#         raise err
+    df = import_data("./data/bank_data.csv")
+    try:
+        perform_eda(df)
+        assert len(os.listdir('./images/eda/') ) > 0
+        logging.info('Images were created: SUCCESS')
+    except AssertionError as err:
+        logging.error("Testing perform_eda: Plots were not created, check your data")
+        raise err
     
-#     try:
-#         data_verification = [os.path.getsize(file)>0 for file in glob.glob('./images/eda/*.png')]
-#         assert any(x == True for x in data_verification)
-#         logging.info('Image were created and not empty: SUCCESS')
-#     except AssertionError as err:
-#         logging.error("Testing perform_eda: Plots were created some has data size 0 kb")    
-#         raise err
+    try:
+        data_verification = [os.path.getsize(file)>0 for file in glob.glob('./images/eda/*.png')]
+        assert any(x == True for x in data_verification)
+        logging.info('Image were created and not empty: SUCCESS')
+    except AssertionError as err:
+        logging.error("Testing perform_eda: Plots were created but some has data size 0 kb")    
+        raise err
 
 def test_encoder_helper(encoder_helper):
     
@@ -148,16 +148,52 @@ def test_perform_feature_engineering(perform_feature_engineering):
                      
                      
 def test_train_models(train_models):
-	'''
-	test train_models
-	'''
-
+    
+    df = import_data("./data/bank_data.csv")
+    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    
+    cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+    ]
+    
+    encoder = encoder_helper(df, cat_columns, 'Churn')
+    
+    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+             'Total_Relationship_Count', 'Months_Inactive_12_mon',
+             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+             'Income_Category_Churn', 'Card_Category_Churn']
+    
+#     X_train, X_test, y_train, y_test = perform_feature_engineering(encoder, keep_cols)
+    
+#     train_models(X_train, X_test, y_train, y_test)
+    try:
+        assert len(os.listdir('./images/results/') ) > 0
+        logging.info('Images of training results were created: SUCCESS')
+    except AssertionError as err:
+        logging.error("train_models: Resulting images were not created, check your data")
+        raise err
+    
+    try:
+        data_verification = [os.path.getsize(file)>0 for file in glob.glob('./images/results/*.png')]
+        assert any(x == True for x in data_verification)
+        logging.info('Images of training results were created and not empty: SUCCESS')
+    except AssertionError as err:
+        logging.error("train_models: Plots were created but some has data size 0 kb")    
+        raise err    
 
 if __name__ == "__main__":
     test_import(import_data)
-#     test_eda(perform_eda)
+    test_eda(perform_eda)
     test_encoder_helper(encoder_helper)
     test_perform_feature_engineering(perform_feature_engineering)
+    test_train_models(train_models)
     
 
 
