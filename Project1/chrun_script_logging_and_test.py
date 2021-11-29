9923808848c45566828ed420c4d780414154f795
tugrulguner <tugrulgunr@gmail.com>
2021-11-28 19:16:39 -0500

    Test and Logging updated

--- Project1/chrun_script_logging_and_test.py
+++ Project1/chrun_script_logging_and_test.py
@@ -2,6 +2,8 @@ import os
 import logging
 from churn_library import *
 import glob
+from sklearn.model_selection import train_test_split
+
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
@@ -83,10 +85,68 @@ def test_encoder_helper(encoder_helper):
 
 
 def test_perform_feature_engineering(perform_feature_engineering):
-
     
+    
+    df = import_data("./data/bank_data.csv")
+    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    
+    cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+    ]
+    
+    encoder = encoder_helper(df, cat_columns, 'Churn')
+    
+    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+             'Total_Relationship_Count', 'Months_Inactive_12_mon',
+             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+             'Income_Category_Churn', 'Card_Category_Churn']
+    
+    X_train, X_test, y_train, y_test = perform_feature_engineering(encoder, keep_cols)
+    
+    try:
+    
+        assert X_train.empty == False
+        assert y_train.empty == False
+        logging.info('Train data was created and not empty: SUCCESS')
 
-
+    except AssertionError as err:
+        logging.error('Train data could not be created')
+        raise err
+     
+    try:
+        assert X_test.empty == False
+        assert y_test.empty == False
+        logging.info('Test data was created and not empty: SUCCESS')
+    
+    except AssertionError as err:
+        logging.error('Test data could not be created')
+        raise err
+                   
+    try:
+        assert X_train.shape[0] == y_train.shape[0]
+        logging.info('Train data shapes match: SUCCESS')
+    
+    except AssertionError as err:
+        logging.error('Train data X and Y doesnt match')
+        raise err
+        
+    try:
+        assert X_test.shape[0] == y_test.shape[0]
+        logging.info('Train data shapes match: SUCCESS')
+    
+    except AssertionError as err:
+        logging.error('Test data X and Y doesnt match')
+        raise err
+                     
+                     
+                     
 def test_train_models(train_models):
 	'''
 	test train_models
@@ -97,6 +157,7 @@ if __name__ == "__main__":
     test_import(import_data)
 #     test_eda(perform_eda)
     test_encoder_helper(encoder_helper)
+    test_perform_feature_engineering(perform_feature_engineering)
     
 
 
